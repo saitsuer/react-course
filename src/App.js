965bff3ba8f314b956602992e231fbb757237a88
saitsuer <saitsuer@gmail.com>
2021-07-05 00:55:17 +0300

    functional components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponent";
+import React, { Component } from 'react';
+import Main from "./components/MainComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container" >
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+
+class App extends Component {
+
+
+  render() {
+
+
+    return (
+      <div className="App" >
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
